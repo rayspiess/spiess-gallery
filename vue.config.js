87f7d5d95149a881d098fc730952182a58e48b2b
Ray Spiess <rhspiess@wisc.edu>
2020-05-23 21:13:04 -0500

    proxy does not fail

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
 module.exports = {
+  //https://res.cloudinary.com/<cloudname>/image/fetch/https://<username>:<password>@<url>
+   //var url  =  'https://917975234378397:FoqQCqQzluD9tI7QZri0Dt_cTto@api.cloudinary.com/v1_1/spiess-co/resources/image';
+   // var = 'http://jsonplaceholder.typicode.com/';
+
     devServer: {
-
-// var url  = 'https://917975234378397:FoqQCqQzluD9tI7QZri0Dt_cTto@api.cloudinary.com/v1_1/spiess-co/resources/image';
-
-
       proxy: {
-        '/api': {
-          target: 'https://917975234378397:FoqQCqQzluD9tI7QZri0Dt_cTto@api.cloudinary.com/v1_1/spiess-co/resources/image',
-          pathRewrite: {'^/api' : ''},
-          secure: false
+        '^/api': {
+          target: 'http://jsonplaceholder.typicode.com/',
+         // changeOrigin: true,
+         // target: 'http://jsonplaceholder.typicode.com/',
+         // secure: false,
+          pathRewrite: {
+           '^/api': '',
+         },
         }
       },
     },
+    
   }
