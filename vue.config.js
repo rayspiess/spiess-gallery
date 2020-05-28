aea4b742524a1c2d3a6314ff717becfa9976ab89
Ray Spiess <rhspiess@wisc.edu>
2020-05-28 17:32:22 -0500

    arrg

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,6 @@ module.exports = {
 
     devServer: {
       proxy: {
-        '/.netlify/functions/images' : {
-          target: 'https://917975234378397:FoqQCqQzluD9tI7QZri0Dt_cTto@api.cloudinary.com/v1_1/spiess-co/resourcesimages/',
-          // changeOrigin: true,
-         // target: 'http://jsonplaceholder.typicode.com/',
-         // secure: false,
-          pathRewrite: {
-            '/.netlify/functions/images' : '',
-         },
         '/api2': {
           target: 'https://917975234378397:FoqQCqQzluD9tI7QZri0Dt_cTto@api.cloudinary.com/v1_1/spiess-co/resources',
           // changeOrigin: true,
